262147f9bf53b74be94c8a8741c8b496aad54f69
HugoLopez <hugo.lopez@etu.umontpellier.fr>
2025-10-13 15:08:12 +0200

    ajout data + initFeatures() dans Trace

--- src/geste/Trace.java
+++ src/geste/Trace.java
@@ -35,7 +35,84 @@ public class Trace implements Featured{
 
 	
 	public void initFeatures() {
-		//todo
+		features = new Vecteur(13);
+		//f1
+		features.set(0, (points.get(2).getX() - points.get(0).getX())/(points.get(0).distance(points.get(2))));
+		//f2
+		features.set(1, (points.get(2).getY() - points.get(0).getY())/(points.get(0).distance(points.get(2))));
+		//calcule bounding box
+		int xmin = points.get(0).getX(), xmax = points.get(0).getX();
+		int ymin = points.get(0).getY(), ymax = points.get(0).getY();
+		for (int i = 1; i < points.size(); i++) {
+			if (points.get(i).getX() < xmin) xmin = points.get(i).getX();
+			if (points.get(i).getX() > xmax) xmax = points.get(i).getX();
+			if (points.get(i).getY() < ymin) ymin = points.get(i).getY();
+			if (points.get(i).getY() > ymax) ymax = points.get(i).getY();
+		}
+		StampedCoord max = new StampedCoord(xmax, ymax);
+		StampedCoord min = new StampedCoord(xmin, ymin);
+		//f3
+		features.set(2, max.distance(min));
+		//f4
+		features.set(3, Math.atan((double)(max.getY() - min.getY())/(max.getX() - min.getX())));
+		//f5
+		features.set(4, points.get(0).distance(points.get(points.size()-1)));
+		//f6
+		features.set(5, (points.get(points.size()-1).getX() - points.get(0).getX())/features.get(4));
+		//f7
+		features.set(6, (points.get(points.size()-1).getY() - points.get(0).getY())/features.get(4));
+		//delta x et delta y
+		int deltapx[] = new int[points.size()-1];
+		int deltapy[] = new int[points.size()-1];
+		for (int i = 0; i < points.size()-1; i++) {
+			deltapx[i] = points.get(i+1).getX() - points.get(i).getX();
+			deltapy[i] = points.get(i+1).getY() - points.get(i).getY();
+		}
+		//f8
+		int sumx = 0;
+		for (int i = 0; i < points.size()-1; i++){
+			sumx += Math.sqrt(deltapx[i]*deltapx[i] + deltapy[i]*deltapy[i]);
+		}
+		features.set(7, sumx);
+		//teta p
+		double teta[] = new double[points.size()-2];
+		for (int i = 1; i < points.size()-1; i++) {
+			teta[i] = Math.atan((double) 
+			(deltapx[i]*deltapy[i-1] - deltapx[i-1]*deltapy[i]) /
+			(deltapx[i]*deltapx[i-1] + deltapy[i]*deltapy[i-1]));
+		}
+		//f9
+		double sumteta = 0;
+		for (int i = 1; i < points.size()-1; i++) {
+			sumteta += teta[i];
+		}
+		features.set(8, sumteta);
+		//f10
+		double sumabsteta = 0;
+		for (int i = 1; i < points.size()-1; i++) {
+			sumabsteta += Math.abs(teta[i]);
+		}
+		features.set(9, sumabsteta);
+		//f11
+		double squaresumteta = 0;
+		for (int i = 1; i < points.size()-1; i++) {
+			squaresumteta += teta[i]*teta[i];
+		}
+		features.set(10, squaresumteta);
+		//delta timestamp
+		long deltaT[] = new long[points.size()-1];
+		for (int i = 0; i < points.size()-1; i++) {
+			deltaT[i] = points.get(i+1).getTimeStamp() - points.get(i).getTimeStamp();
+		}
+		//f12
+		double maxSpeed = 0;
+		for (int i = 0; i < points.size()-1; i++) {
+			double res = (double) ((deltapx[i]*deltapx[i] + deltapy[i]*deltapy[i]) / deltaT[i]*deltaT[i]);
+			if (res > maxSpeed) maxSpeed = res;
+		}
+		features.set(11, maxSpeed);
+		//f13
+		features.set(12, points.get(points.size()-1).getTimeStamp() - points.get(0).getTimeStamp());
 	}
 
 
